903d1b8f56d7b745829ad1ac4fc464b72a6f9060
Tamanna2112 <71064377+Tamanna2112@users.noreply.github.com>
2020-12-15 19:26:29 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,17 @@
+class Box {
+    constructor (x,y,width,height){
+        var options = {
+            'restitution' : 1.0
+        }
+        this.width = width;
+        this.height = height;
+        this.body = Bodies.rectangle (x,y,width,height,options);
+        World.add(world, this.body);
+    }
+    display(){
+        var position = this.body.position;
+        rectMode(CENTER);
+        fill(255);
+        rect(position.x,position.y,this.width,this.height);
+    }
+}
